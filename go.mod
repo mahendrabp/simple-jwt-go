ffc14d2c0a16353a35471aa055d7c085490f6c72
mahendrabp <chipxitro@gmail.com>
2021-11-09 10:14:48 +0700

    feat: add user_usecase

--- go.mod
+++ go.mod
@@ -4,11 +4,12 @@ go 1.15
 
 require (
 	github.com/go-playground/validator/v10 v10.9.0
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/google/uuid v1.3.0
 	github.com/jmoiron/sqlx v1.3.4
 	github.com/labstack/echo/v4 v4.6.1
 	github.com/lib/pq v1.10.4
-	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/viper v1.9.0
 	golang.org/x/crypto v0.0.0-20211108221036-ceb1ce70b4fa
 )
--- pkg/domain/user_usecase.go
+++ pkg/domain/user_usecase.go
@@ -1,1 +1,25 @@
 package domain
+
+import (
+	"simple-jwt-go/pkg/utils"
+
+	"github.com/google/uuid"
+)
+
+type (
+	JWTUseCase interface {
+		Auth(user *User) (*AuthUser, error)
+		GetToken(id uuid.UUID, tokenID uuid.UUID) (uuid.UUID, error)
+		DeleteToken(id uuid.UUID, tokenID uuid.UUID) error
+		Logout(id uuid.UUID, tokenID *utils.TokenDetails) error
+	}
+
+	UserUseCase interface {
+		GetByID(id uuid.UUID) (User, error)
+		Login(user *User) (*AuthUser, error)
+		Store(user *User) (*AuthUser, error)
+		Update(user *User) (*User, error)
+		Delete(id uuid.UUID) error
+		JWTUseCase
+	}
+)
